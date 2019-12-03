7bc09ba952acc7eb5f484b9be71420ef612905b1
Daniel Bacher <mail@phill93.de>
2019-12-03 13:54:58 +0100

    add more infromations

--- files/update-alfred.py
+++ files/update-alfred.py
@@ -4,6 +4,8 @@ import socket
 import netifaces as ni
 import json
 import subprocess
+import os
+import time
 
 hostname = socket.gethostname()
 ip = ni.ifaddresses('bat0')[2][0]['addr']
@@ -13,13 +15,23 @@ if 'eth0' in ni.interfaces():
 else:
     role = "inner"
 
+if os.path.exists("/run/discovery"):
+    discovery = True
+else:
+    discovery = False
+
+model = open("/sys/firmware/devicetree/base/model").read()
+
 data = {
     'hostname': hostname,
     'ipaddr': ip,
-    'role': role
+    'role': role,
+    'discovery': discovery,
+    'model': model,
+    'time': time.time()
 }
 
-process = subprocess.Popen(["alfred", "-s 64"], stdin=subprocess.PIPE)
+process = subprocess.Popen(["alfred", "-s 64"], stdin=subprocess.PIPE, encoding='utf-8')
 
 process.stdin.write(json.dumps(data).encode())
 process.stdin.close()
